4347618c93bf471f4abd37b853dbd5affb4c41f0
Aaron Tandem <aaronlbiever@gmail.com>
2024-01-10 16:15:41 +0900

    begins creating distanceBetween() functionality

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import hash_table_class
-import package_class
 import methods
-import csv
 
 # instance of the self-adjusting data structure, followed by a method call to load it with package data
 packageHashTable = hash_table_class.ChainingHashTable()
@@ -14,17 +12,26 @@ methods.loadDistanceData("WGUPS_distance_table.csv", distanceDataList)
 addressDataList = []
 methods.loadAddressData("WGUPS_distance_table.csv", addressDataList)
 
+def distanceBetween(address1, address2):
+    return distanceDataList[address1][address2]
+
+print("Address Data List Index of String Value:")
+print(addressDataList.index('Salt Lake County/United Police Dept 3365 S 900 W'))
+
+# print(distanceBetween(1, 1))
+
+# print(distanceDataList[4][2])
+
 # display the hash table data to the console
 # print("Package Data from Hashtable:")
 # # Fetch data from Hash Table
 # for i in range(len(packageHashTable.table)):
 #     print("Package: {}".format(packageHashTable.search(i+1)))
 
-
-print("Distance Data List:")
-print(len(distanceDataList))
-print(distanceDataList)
-
-print("Address Data List:")
-print(len(addressDataList))
-print(addressDataList)
+# print("Distance Data List:")
+# print(len(distanceDataList))
+# print(distanceDataList)
+#
+# print("Address Data List:")
+# print(len(addressDataList))
+# print(addressDataList)
