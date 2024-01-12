9938e55cc1d34bd7efe0564a84ec349251f68d0e
Aaron Tandem <aaronlbiever@gmail.com>
2024-01-12 01:51:30 -1000

    Modifies distanceBetween() method to 'flip' indexes to account for out-of-range instances

--- main.py
+++ main.py
@@ -13,20 +13,39 @@ addressDataList = []
 methods.loadAddressData("WGUPS_distance_table.csv", addressDataList)
 
 def distanceBetween(address1, address2):
-    return distanceDataList[addressDataList.index(address1)][addressDataList.index(address2)]
+    indexAddress1 = addressDataList.index(address1)
+    indexAddress2 = addressDataList.index(address2)
 
-index1 = addressDataList.index('3365 S 900 W')
-index2 = addressDataList.index('177 W Price Ave')
+    # because our 2D list is a series of arrays of i++ size, 'coordinates' will only work unidirectionally
+    # because coordinates should be reversible, logic should be provided to account for such cases
+    # So, this 'flips' the indexes so that the index reference never exceeds the ADT bounds
+    if indexAddress2 > indexAddress1:
+        tempIndex = indexAddress1
+        flippedIndexAddress1 = indexAddress2
+        flippedIndexAddress2 = tempIndex
+        return distanceDataList[flippedIndexAddress1][flippedIndexAddress2]
+
+    else:
+        return distanceDataList[indexAddress1][indexAddress2]
+
+firstAddress = '2835 Main St'
+index1 = addressDataList.index(firstAddress)
+
+secondAddress = '3060 Lester St'
+index2 = addressDataList.index(secondAddress)
 
 print("Address Data List Index of String Value:")
 print(index1)
 print(index2)
 
-print("Output of distanceBetween Function:")
-print(distanceBetween('3365 S 900 W', '177 W Price Ave'))
+print("Output of distanceBetween Function for:", firstAddress, "to", secondAddress)
+print(distanceBetween(firstAddress, secondAddress))
 
-print("Output from distanceDataList:")
-print(distanceDataList[index1][index2])
+print("Output of distanceBetween Function for:", secondAddress, "to", firstAddress)
+print(distanceBetween(secondAddress, firstAddress))
+
+# print("Output from distanceDataList:", index1, "-", index2)
+# print(distanceDataList[index1][index2])
 
 # display the hash table data to the console
 # print("Package Data from Hashtable:")
@@ -34,9 +53,9 @@ print(distanceDataList[index1][index2])
 # for i in range(len(packageHashTable.table)):
 #     print("Package: {}".format(packageHashTable.search(i+1)))
 
-print("Distance Data List:")
-print("Length:", len(distanceDataList))
-print(distanceDataList)
+# print("Distance Data List:")
+# print("Length:", len(distanceDataList))
+# print(distanceDataList)
 
 # print("Address Data List:")
 # print("Length:", len(addressDataList))
