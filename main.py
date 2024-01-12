63272513b64476f9aae52d7e3a4fca0828516f28
Aaron Tandem <aaronlbiever@gmail.com>
2024-01-12 02:01:55 -1000

    Moves distanceBetween() function to methods.py and includes arguments for calling the appropriate lists

--- main.py
+++ main.py
@@ -12,22 +12,6 @@ methods.loadDistanceData("WGUPS_distance_table.csv", distanceDataList)
 addressDataList = []
 methods.loadAddressData("WGUPS_distance_table.csv", addressDataList)
 
-def distanceBetween(address1, address2):
-    indexAddress1 = addressDataList.index(address1)
-    indexAddress2 = addressDataList.index(address2)
-
-    # because our 2D list is a series of arrays of i++ size, 'coordinates' will only work unidirectionally
-    # because coordinates should be reversible, logic should be provided to account for such cases
-    # So, this 'flips' the indexes so that the index reference never exceeds the ADT bounds
-    if indexAddress2 > indexAddress1:
-        tempIndex = indexAddress1
-        flippedIndexAddress1 = indexAddress2
-        flippedIndexAddress2 = tempIndex
-        return distanceDataList[flippedIndexAddress1][flippedIndexAddress2]
-
-    else:
-        return distanceDataList[indexAddress1][indexAddress2]
-
 firstAddress = '2835 Main St'
 index1 = addressDataList.index(firstAddress)
 
@@ -39,10 +23,10 @@ print(index1)
 print(index2)
 
 print("Output of distanceBetween Function for:", firstAddress, "to", secondAddress)
-print(distanceBetween(firstAddress, secondAddress))
+print(methods.distanceBetween(firstAddress, secondAddress, addressDataList, distanceDataList))
 
 print("Output of distanceBetween Function for:", secondAddress, "to", firstAddress)
-print(distanceBetween(secondAddress, firstAddress))
+print(methods.distanceBetween(secondAddress, firstAddress, addressDataList, distanceDataList))
 
 # print("Output from distanceDataList:", index1, "-", index2)
 # print(distanceDataList[index1][index2])
