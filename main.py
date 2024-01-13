ff9a7d15886db2ba3c0454bf78b66b6d71cb2dda
Aaron Tandem <aaronlbiever@gmail.com>
2024-01-12 18:45:36 -1000

    Modifies TruckClass to easily append packages to its native list from the hash table and print said list to the console

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import hash_table_class
+import truck_class
 import methods
 
 # instance of the self-adjusting data structure, followed by a method call to load it with package data
@@ -12,30 +13,41 @@ methods.loadDistanceData("WGUPS_distance_table.csv", distanceDataList)
 addressDataList = []
 methods.loadAddressData("WGUPS_distance_table.csv", addressDataList)
 
-firstAddress = '2835 Main St'
-index1 = addressDataList.index(firstAddress)
+# firstAddress = '2835 Main St'
+# index1 = addressDataList.index(firstAddress)
+#
+# secondAddress = '3060 Lester St'
+# index2 = addressDataList.index(secondAddress)
+#
+# print("Address Data List Index of String Value:")
+# print(index1)
+# print(index2)
+#
+# print("Output of distanceBetween Function for:", firstAddress, "to", secondAddress)
+# print(methods.distanceBetween(firstAddress, secondAddress, addressDataList, distanceDataList))
+#
+# print("Output of distanceBetween Function for:", secondAddress, "to", firstAddress)
+# print(methods.distanceBetween(secondAddress, firstAddress, addressDataList, distanceDataList))
 
-secondAddress = '3060 Lester St'
-index2 = addressDataList.index(secondAddress)
+truck1 = truck_class.Truck()
+truck1.addPackage(packageHashTable.search(1))
+truck1.addPackage(packageHashTable.search(20))
+truck1.printPackageList(1)
 
-print("Address Data List Index of String Value:")
-print(index1)
-print(index2)
-
-print("Output of distanceBetween Function for:", firstAddress, "to", secondAddress)
-print(methods.distanceBetween(firstAddress, secondAddress, addressDataList, distanceDataList))
-
-print("Output of distanceBetween Function for:", secondAddress, "to", firstAddress)
-print(methods.distanceBetween(secondAddress, firstAddress, addressDataList, distanceDataList))
-
+truck2 = truck_class.Truck()
+truck2.addPackage(packageHashTable.search(3))
+truck2.addPackage(packageHashTable.search(18))
+truck2.addPackage(packageHashTable.search(36))
+truck2.addPackage(packageHashTable.search(38))
+truck2.printPackageList(2)
 # print("Output from distanceDataList:", index1, "-", index2)
 # print(distanceDataList[index1][index2])
 
 # display the hash table data to the console
-# print("Package Data from Hashtable:")
-# # Fetch data from Hash Table
-# for i in range(len(packageHashTable.table)):
-#     print("Package: {}".format(packageHashTable.search(i+1)))
+print("Package Data from Hashtable:")
+# Fetch data from Hash Table
+for i in range(len(packageHashTable.table)):
+    print("Package: {}".format(packageHashTable.search(i+1)))
 
 # print("Distance Data List:")
 # print("Length:", len(distanceDataList))
--- truck_class.py
+++ truck_class.py
@@ -1,9 +1,17 @@
 class Truck:
-    def __init__(self, packages):
-        #IDEA: Perhaps store the packages as a list here??
-        self.packages = [packages]
+    def __init__(self):
+        self.packages = []
 
-    def __str__(self):  # overwrite print(Movie) otherwise it will print object reference
-        return "%s, %s, %s, %s, %s" % (self.ID, self.name, self.year, self.price, self.status)
+    # def __str__(self):  # overwrite print(Movie) otherwise it will print object reference
+    #     return "%s, %s, %s, %s, %s" % (self.ID, self.name, self.year, self.price, self.status)
 
-    #will likely need a method that 'removes' a package from the list once its been delivered
+    #will likely need a method that 'removes' a package from the list once it's been delivered
+
+    # TODO: Create addPackage() method to append to packages[]
+    def addPackage(self, package):
+        self.packages.append(package)
+
+    def printPackageList(self, truckNumber):
+        print("List of Packages for Truck #" + str(truckNumber))
+        for package in self.packages:
+            print(package)
